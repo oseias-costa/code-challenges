80b6943264c65856906578faf54e924b3c0707ca
Oséias Costa <oseiasc2@gmail.com>
2023-07-10 02:05:01 +0000

    feat(03-romanToInt.js): challenge completed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const { isPalindrome } = require("./leetcode/02-isPalindrome.js");
-const { miniMaxSum } = require("./hakerRank/01-miniMaxSum.js");
+const { romanToInt } = require("./leetcode/03-romanToInt.js");
+// const { miniMaxSum } = require("./hakerRank/01-miniMaxSum.js");
 
-console.log("isPalindrome", miniMaxSum([1, 3, 5, 7, 9]));
+console.log("romanToInt", romanToInt("XIV"));
